55f332329731d8061a62e4bcb2955beaa7baf93d
pr0n1x <rootfavell@gmail.com>
2013-06-04 19:05:14 +0800

    ...

--- install/modules/a68.core/install/components/a68/layout/component.php
+++
@@ -1,9 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-if( !CModule::IncludeModule('a68.core') ) {
-	ShowError(GetMessage('A68_CORE_IS_NOT_INSTALLED'));
-	return;
-}
-
-$this->IncludeComponentTemplate();
-?>
--- install/modules/a68.core/lang/ru/classes/DBSimple.php
+++
@@ -1,8 +0,0 @@
-<?php
-$MESS["A68_DB_SIMPLE_ERR_NOTHING_TO_DELETE"] = "Ошибка удаления. Запись не найдена.";
-$MESS["A68_DB_SIMPLE_ERR_ADD_DUP_PK"] = "Ошибка создания записи. Запись со значением поля #PK_NAME# = «#PK_VALUE#» уже существует";
-$MESS["A68_DB_SIMPLE_ERR_ADD_DUP_UNIQUE"] = "Ошибка создания. Запись со значением уникального индекса (#FLD_LIST#) = (#FLD_VALUES#) уже существует";
-$MESS["A68_DB_SIMPLE_ERR_UPD_DUP_UNIQUE"] = "Ошибка обновления. Запись со значением уникального индекса (#FLD_LIST#) = (#FLD_VALUES#) уже существует";
-$MESS["A68_DB_SIMPLE_ERR_ADD_MISS_FIELD"] = "Не заполено (или неверно) обязательное поле «#FIELD_NAME#» при создании записи";
-$MESS["A68_DB_SIMPLE_ERR_UPD_NOTHING_TO_UPDATE"] = "Ошибка обновления. Запись не найдена";
-?>
