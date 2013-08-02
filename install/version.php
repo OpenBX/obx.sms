01aed7801b88ff03e0f23435b6796c8a00927a43
pr0n1x <rootfavell@gmail.com>
2013-08-03 00:38:37 +0800

    МаркетПлейсу походу нужно именно такое название переменной...

--- install/version.php
+++ install/version.php
@@ -5,8 +5,8 @@
  ** @mailto info@a-68.ru                **
  *****************************************/
 
-$arModuleInfo = array(
+$arModuleVersion = array(
 	"VERSION" => "0.9.2",
 	"VERSION_DATE" => "2013-08-01",
 );
-return $arModuleInfo;
+return $arModuleVersion;
