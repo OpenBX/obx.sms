4e6ed55590c2904e3c26d0b680d90bb81ace5493
pr0n1x <rootfavell@gmail.com>
2013-09-10 00:06:21 +0800

    ...

--- install/version.php
+++ install/version.php
@@ -6,8 +6,8 @@
  *****************************************/
 
 $arModuleVersion = array(
-	"VERSION" => "1.0.0",
-	"VERSION_DATE" => "2013-08-06",
+	"VERSION" => "1.0.1",
+	"VERSION_DATE" => "2013-09-09",
 );
 return $arModuleVersion;
 
