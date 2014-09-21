60c5f29728379df3b39d1f42c5c0a6f172dc4c54
pr0n1x <rootfavell@gmail.com>
2014-09-21 12:05:56 +0800

    Скорректировал ссылку разработчика

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -2,5 +2,5 @@
 $MESS["OBX_SMS_MODULE_INSTALL_NAME"] = "OpenBX: СМС-мастер";
 $MESS["OBX_SMS_MODULE_INSTALL_DESCRIPTION"] = 'Модуль мастерит СМС';
 $MESS["OBX_SMS_PARTNER_NAME"] = "OpenBitrixSolutions";
-$MESS["OBX_SMS_PARTNER_URI"] = "http://www.a-68.ru";
+$MESS["OBX_SMS_PARTNER_URI"] = "http://www.devtop.ru";
 ?>
