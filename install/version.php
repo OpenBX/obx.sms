c1783a5bdbf21b3ff11c9aad222b777e500d292f
pr0n1x <rootfavell@gmail.com>
2014-12-29 01:28:48 +0700

    Новая версия с обновленным obx.core

--- install/version.php
+++ install/version.php
@@ -9,7 +9,7 @@
  *******************************************/
 
 $arModuleVersion = array(
-	"VERSION" => "1.3.1",
+	"VERSION" => "1.3.2",
 	"VERSION_DATE" => "2072-02-28",
 );
 return $arModuleVersion;
